b6f06e8b6de247af840f1cb8e1122e8fd188f605
trongbc <152123622+trongbc@users.noreply.github.com>
2023-11-27 09:24:01 +0700

    Create .lua

---
+++ .lua
@@ -0,0 +1,16 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+getgenv().Config = {
+    ["StartFarmChest"] = true,
+    ["Team"] = "Marines",
+    ["TimeReset"] = 5, -- No Edit
+    ["White Screen"] = false,
+    ["Stop The God's Chalice"] = true,
+    ["Stop The Fist of Darkness"] = true,
+    ["HopServer"] = {
+        ["Enable"] = true,
+        ["BypassServer"] = true,
+        ["Region"] = "Singapore"
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trongbc/Free-Script/main/Chest-Farm.lua"))()
